ded8a0fc9d3268fb52bf3bec4ef2f0476b85a45f
BoomBone <ticccccc@163.com>
2017-05-23 13:48:33 +0800

    实现个人中心Fragment名字、昵称、头像显示

--- superwechar/src/cn/ucai/superwechar/ui/fragment/MeFragment.java
+++ superwechar/src/cn/ucai/superwechar/ui/fragment/MeFragment.java
@@ -5,18 +5,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.Unbinder;
+import cn.ucai.easeui.domain.User;
 import cn.ucai.easeui.ui.EaseBaseFragment;
 import cn.ucai.superwechar.R;
-import cn.ucai.superwechar.ui.MainActivity;
+import cn.ucai.superwechar.SuperWeChatHelper;
 
 
 /**
@@ -25,10 +30,13 @@ import cn.ucai.superwechar.ui.MainActivity;
 public class MeFragment extends EaseBaseFragment {
 
 
-    @BindView(R.id.txt_title)
-    TextView txtTitle;
+    @BindView(R.id.iv_profile_avatar)
+    ImageView ivProfileAvatar;
+    @BindView(R.id.tv_profile_nickname)
+    TextView tvProfileNickname;
+    @BindView(R.id.tv_profile_username)
+    TextView tvProfileUsername;
     Unbinder unbinder;
-
     public MeFragment() {
         // Required empty public constructor
     }
@@ -46,7 +54,6 @@ public class MeFragment extends EaseBaseFragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        txtTitle.setVisibility(View.VISIBLE);
     }
 
     @Override
@@ -56,20 +63,41 @@ public class MeFragment extends EaseBaseFragment {
 
     @Override
     protected void setUpView() {
+        titleBar.setRightImageResource(R.drawable.em_add);
+        titleBar.setTitle(getString(R.string.me));
+        User user = SuperWeChatHelper.getInstance().getUserProfileManager().getCurrentAppUserInfo();
+        if (user!=null){
+            tvProfileNickname.setText(user.getMUserNick());
+            tvProfileUsername.setText("微信号: " +user.getMUserName());
+            if(!TextUtils.isEmpty(user.getAvatar())){
+                Glide.with(getContext()).load(user.getAvatar()).placeholder(R.drawable.em_default_avatar).into(ivProfileAvatar);
+            }else{
+                Glide.with(getContext()).load(R.drawable.em_default_avatar).into(ivProfileAvatar);
+            }
+        }
 
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        if(unbinder!=null){
+        if (unbinder != null) {
             unbinder.unbind();
         }
 
     }
 
-    @OnClick(R.id.tv_profile_settings)
-    public void onSettingClicked() {
-        startActivity(new Intent(getContext(),SettingsActivity.class));
+
+    @OnClick({R.id.layout_profile_view, R.id.tv_profile_money, R.id.tv_profile_settings})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.layout_profile_view:
+                break;
+            case R.id.tv_profile_money:
+                break;
+            case R.id.tv_profile_settings:
+                startActivity(new Intent(getContext(), SettingsActivity.class));
+                break;
+        }
     }
 }
