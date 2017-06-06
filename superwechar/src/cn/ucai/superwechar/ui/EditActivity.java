1cb99ae1c0dab220becd909fd5e59a115ea5eb6c
BoomBone <ticccccc@163.com>
2017-06-06 17:33:58 +0800

    公开群搜索风格统一并显示群组图标

--- superwechar/src/cn/ucai/superwechar/ui/EditActivity.java
+++ superwechar/src/cn/ucai/superwechar/ui/EditActivity.java
@@ -8,33 +8,43 @@ import android.widget.TextView;
 
 import cn.ucai.superwechar.R;
 
-public class EditActivity extends BaseActivity{
-	private EditText editText;
+public class EditActivity extends BaseActivity {
+    private EditText editText;
 
 
-	@Override
-	protected void onCreate(Bundle arg0) {
-		super.onCreate(arg0);
-		setContentView(R.layout.em_activity_edit);
-		
-		editText = (EditText) findViewById(R.id.edittext);
-		String title = getIntent().getStringExtra("title");
-		String data = getIntent().getStringExtra("data");
-		if(title != null)
-			((TextView)findViewById(R.id.tv_title)).setText(title);
-		if(data != null)
-			editText.setText(data);
-		editText.setSelection(editText.length());
-		
-	}
-	
-	
-	public void save(View view){
-		setResult(RESULT_OK,new Intent().putExtra("data", editText.getText().toString()));
-		finish();
-	}
+    @Override
+    protected void onCreate(Bundle arg0) {
+        setContentView(R.layout.em_activity_edit);
+        super.onCreate(arg0);
+        showLeftBack();
+        setListener();
+        editText = (EditText) findViewById(R.id.edittext);
+        String title = getIntent().getStringExtra("title");
+        String data = getIntent().getStringExtra("data");
+        if (title != null)
+            ((TextView) findViewById(R.id.tv_title)).setText(title);
+        if (data != null)
+            editText.setText(data);
+        editText.setSelection(editText.length());
 
-	public void back(View view) {
-		finish();
-	}
+    }
+
+    private void setListener() {
+        titleBar.getRightLayout().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                save(v);
+            }
+        });
+    }
+
+
+    public void save(View view) {
+        setResult(RESULT_OK, new Intent().putExtra("data", editText.getText().toString()));
+        finish();
+    }
+
+    public void back(View view) {
+        finish();
+    }
 }
